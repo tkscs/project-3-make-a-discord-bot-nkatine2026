142b61f4f4139ad1e2cf884fd4e03cb018086e49
nkatine2026 <nkatine2026@kehillahstudent.org>
2026-03-05 08:38:13 -0800

    comein3

--- my_bot.py
+++ my_bot.py
@@ -21,7 +21,7 @@ def should_i_respond(user_message, user_name):
     return True
   elif "AHHH":
      return True
-  elif "Can you say anything else?!?":
+  elif "Can you say antything else?":
      return True
   elif "THE ISLAND?!?":
      return True
@@ -45,7 +45,37 @@ This function will be called every time the `should_i_respond` function returns 
 """
 
 def respond(user_message, user_name):
-    c =["NOAH's NOT ON THE LIST", "Noah did no wrong", "Why would assume he's on it?!?", "Noah isn't like that"]
-    w = random.choice(c)
-    return w
-    {user_message.replace("BOT_NOAH", user_name)}
+   c =["NOAH's NOT ON THE LIST", "Noah did no wrong", "Why would assume he's on it?!?", "Noah isn't like that"]
+   d = ["hahaha", "your pain feeds me", "I'm enjoying your suffering"]
+   e = ["damn you got him", "he is on the list but did no wrong", "He was a reporter"]
+   if "umm ok?" in user_message:
+         z = random.choice(c)
+         return z
+   elif "I didn't say you were" in user_message:
+      w = random.choice(c)
+      return w
+   elif  "calm down" in user_message:
+      l = random.choice(c)
+      return l
+   elif "OKAY YOU'RE CLEARLY ON IT!!!" in user_message:
+      y = random.choice(e)
+      return y
+   elif "Can you say antything else?" in user_message:
+      x = random.choice(d)
+      return x
+   elif "AHHH" in user_message in user_message: 
+      g = random.choice(d)
+      return g
+   elif "umm ok?" in user_message:
+      ha = random.choice(c)
+      return ha
+   elif  "Bro Really" in user_message:
+      zed = random.choice(c)
+      return zed
+   elif "Care to explain the photo" in user_message:
+      yy = random.choice(e)
+      return yy
+   elif "THE ISLAND?!?" in user_message:
+      yyy = random.choice(e)
+      return yyy
+   {user_message.replace("BOT_NOAH", user_name)}
